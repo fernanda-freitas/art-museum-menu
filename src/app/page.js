57810873beb31c4032276837f3cbd51e7e7bdbdc
Fernanda Freitas <idea.fernanda@gmail.com>
2023-08-21 16:53:04 +0100

    Completed layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,128 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import logotype from "../../public/images/logotype.svg";
+import logotypered from "../../public/images/logotypered.svg";
+import image01 from "../../public/images/image01.png";
+import image03 from "../../public/images/image03.jpg";
+import typography from "../../public/images/image02.svg";
+import menubg01 from "../../public/images/menubg01.svg";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [isOpen, setIsOpen] = useState(false);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const menu = [
+    {
+      title: "The museum",
+      image: "image03.jpg",
+    },
+    {
+      title: "Exhibitions",
+      image: "image03.jpg",
+    },
+    {
+      title: "Collections",
+      image: "image03.jpg",
+    },
+    {
+      title: "Public",
+      image: "image03.jpg",
+    },
+  ];
+
+  return (
+    <main className="relative h-screen w-screen overflow-hidden bg-creme">
+      {/* 1rd layer */}
+      <div className="absolute w-full left-0 top-0 z-20 flex justify-between p-8">
+        <Image src={logotype} alt="logotype" />
+        <span
+          onClick={handleOpen}
+          className="uppercase text-base hover:cursor-pointer"
+        >
+          Menu
+        </span>
+      </div>
+      <div className="absolute -top-24 -right-4 w-screen h-screen z-10">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={typography}
+          alt="exhibition information"
+          className="w-full h-full"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="hidden md:inline absolute -bottom-52 left-1/2 -translate-x-1/2">
+        <Image
+          src={image01}
+          alt="exhibition information"
+          style={{ minWidth: 400, minHeight: 300 }}
+          className="ovject-fill"
+        />
       </div>
+      {/* 2rd layer */}
+      {isOpen && (
+        <>
+          <div className="absolute inset-0 bottom-16 z-30">
+            <div className="absolute bottom-6 right-8 flex items-center gap-x-8 z-30">
+              <span className=" text-xl font-light text-crimson color-crimson">
+                Work with us
+              </span>
+              <button
+                type="button"
+                className="text-pink bg-crimson px-5 py-2 rounded-full"
+              >
+                Apply
+              </button>
+            </div>
+            <Image
+              src={menubg01}
+              alt="menu background"
+              className="bg-pink object-cover"
+              fill
+            />
+          </div>
+          {/* 3rd layer */}
+          <div className="absolute inset-0 bottom-40 bg-creme z-40">
+            <div className="absolute w-full left-0 top-0 z-20 flex justify-between p-8">
+              <Image src={logotypered} alt="logotype" />
+              <span
+                onClick={handleClose}
+                className="uppercase text-base hover:cursor-pointer text-crimson"
+              >
+                Menu
+              </span>
+            </div>
+            <div className="flex w-full h-full">
+              <ul className="md:w-1/2 pt-44 px-8">
+                {menu.map((item, i) => {
+                  return (
+                    <li
+                      className="font-reailge text-6xl lg:text-custom leading-snug text-crimson opacity-60 hover:opacity-100 hover:cursor-pointer"
+                      key={i}
+                    >
+                      {item.title}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="relative hidden md:inline-flex md:w-1/2 px-8 border-l border-opacity-30 border-crimson">
+                <Image
+                  src={image03}
+                  alt="menu image"
+                  fill
+                  className="py-8 px-40 object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        reailge: ["var(--font-reailge)"],
+      },
+      fontSize: {
+        custom: "5vw",
+      },
+      colors: {
+        creme: "#FFFCF5",
+        pink: "#FFE4E4",
+        crimson: "#A14545",
       },
     },
   },
   plugins: [],
-}
+};
