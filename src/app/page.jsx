99cb520d54afaaa5f2140be92d68a5b953dc4e42
Fernanda Freitas <idea.fernanda@gmail.com>
2023-08-22 12:43:33 +0100

    Clean code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,63 +13,6 @@ export default function Home() {
     setIsOpen(!isOpen);
   };
 
-  const menu = [
-    {
-      title: "The museum",
-      image: "image03.jpg",
-    },
-    {
-      title: "Exhibitions",
-      image: "image03.jpg",
-    },
-    {
-      title: "Collections",
-      image: "image03.jpg",
-    },
-    {
-      title: "Public",
-      image: "image03.jpg",
-    },
-  ];
-
-  const firstLayer = {
-    initial: { y: -1000 },
-    enter: {
-      y: 0,
-      transition: {
-        duration: 0.8,
-        ease: [0.08, 0.65, 0.53, 0.96],
-      },
-    },
-    exit: {
-      y: -1000,
-      transition: {
-        duration: 0.8,
-        delay: 0.1,
-        ease: [0.08, 0.65, 0.53, 0.96],
-      },
-    },
-  };
-
-  const secondLayer = {
-    initial: { y: -1000 },
-    enter: {
-      y: 0,
-      transition: {
-        delay: 0.2,
-        duration: 0.8,
-        ease: [0.08, 0.65, 0.53, 0.96],
-      },
-    },
-    exit: {
-      y: -1000,
-      transition: {
-        duration: 0.8,
-        ease: [0.08, 0.65, 0.53, 0.96],
-      },
-    },
-  };
-
   return (
     <main className="relative h-screen w-screen overflow-hidden bg-creme">
       <Nav handleMenu={handleMenu} />
